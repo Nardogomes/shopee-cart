ff0b8d7907d14e0364a932ed51c4f90dd2a67c36
Ednardo Gomes Dos Santos <ednardo.santos@lanlink.com.br>
2025-06-24 11:57:41 -0300

    feat: implement function add Cart, calculate subtotal e add whish list.

--- src/services/cart.js
+++ src/services/cart.js
@@ -1,7 +1,13 @@
-async function addItem(userCart) {}
+async function addItem(userCart, item) {
+  userCart.push(item)
+}
+
+async function calculateTotal(userCart) {
+  console.log(userCart.reduce((total, item) => total + item.subtotal(), 0))
+}
 
 async function deleteItem(userCart, name) {}
 
 async function removeItem(userCart, index) {}
 
-async function calculateTotal(userCart) {}
+export {addItem, calculateTotal, deleteItem, removeItem}

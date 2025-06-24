beccd2ad2985933c5af195fa7c3f9bc23c589598
Ednardo Gomes Dos Santos <ednardo.santos@lanlink.com.br>
2025-06-24 13:53:44 -0300

    feat: implement delete item function.

--- src/services/cart.js
+++ src/services/cart.js
@@ -6,7 +6,13 @@ async function calculateTotal(userCart) {
   console.log(userCart.reduce((total, item) => total + item.subtotal(), 0))
 }
 
-async function deleteItem(userCart, name) {}
+async function deleteItem(userCart, name) {
+  const index = userCart.findIndex((item) => item.name === name)
+
+  if(index !== -1) {
+    userCart.splice(index, 1)
+  }
+}
 
 async function removeItem(userCart, index) {}
 

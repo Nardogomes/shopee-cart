7ff674d6183511e9b5e5242487e62bdb6b0ccd50
Ednardo Gomes Dos Santos <ednardo.santos@lanlink.com.br>
2025-06-24 21:14:58 -0300

    feat: implement remove item function.

--- src/index.js
+++ src/index.js
@@ -6,11 +6,14 @@ const myWhishList = []
 
 console.log("Bem vindo ao seu carrinho de compras!")
 
-const item1 = await createItem("figure-action A", 70.00, 2)
+const item1 = await createItem("figure-action A", 70.00, 3)
 const item2 = await createItem("figure-action B", 90.50, 1)
 
 await addItem(myCart, item1)
 await addItem(myCart, item2)
+
+await removeItem(myCart, item1)
+
 await displayCart(myCart)
 
 // await deleteItem(myCart, item2.name)
